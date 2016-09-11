8aceb43e4326bf802bffe1bd22e0939f84a6158c
cev136 <cev136@gmail.com>
2016-09-11 12:53:14 -0400

    added comments to TriggerSoundTest
    This is an example showing how to play a sound file on the robot
    controller phone.

--- Team11096Code/src/main/java/org/firstinspires/ftc/team11096code/triggerSoundTest.java
+++ Team11096Code/src/main/java/org/firstinspires/ftc/team11096code/triggerSoundTest.java
@@ -1,10 +1,13 @@
+// this OpMode makes a puppy barking sound when you press the "a" button on the gamepad.
+// you have to create the sound resource by inserting a sound file into the src/main/res/raw folder
+
 package org.firstinspires.ftc.team11096code;
 
 //import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 //import com.qualcomm.robotcore.util.Range;
-import android.media.MediaPlayer;
+import android.media.MediaPlayer; // include this class to play a sound file on the robot phone
 
 @TeleOp(name="K9bot: Telop Bark", group="K9bot")
 //@Disabled
@@ -12,15 +15,17 @@ public class triggerSoundTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        final MediaPlayer mp;
+        final MediaPlayer mp; // instantiate the object name
+        // populate the MediaPlayer object. The resource should be in the res/raw folder
+        // and should be named "puppybarking.wav"
         mp = MediaPlayer.create(hardwareMap.appContext, R.raw.puppybarking);
         waitForStart();
 
         while (opModeIsActive()) {
 
-            // Use gamepad Y & A raise and lower the arm
+            // Use gamepad A to play the sound
             if (gamepad1.a){
-                mp.start();
+                mp.start(); // this plays the sound
             }
 
             idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop

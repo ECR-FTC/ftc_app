41e1afeb9a74e48aed2b29cb0e2159e1835a9d5d
ECR-FTC <curtis.volin@eastcobbrobotics.org>
2017-03-05 18:09:13 -0500

    This is the step that uses the PID for the shoot motor.

--- StepRunner/src/main/java/org/firstinspires/ftc/teamcode/steprunner/PIDShootStep.java
+++ StepRunner/src/main/java/org/firstinspires/ftc/teamcode/steprunner/PIDShootStep.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.steprunner;
 
+
 /**
  * Created by ECR FTC on 1/15/2017.
  *
@@ -8,19 +9,21 @@ package org.firstinspires.ftc.teamcode.steprunner;
  */
 
 public class PIDShootStep extends Step {
-    static final double DEFAULT_TUP    = 2000;
-    static final double DEFAULT_TDOWN  = 2000;
-    static final double DEFAULT_MIN    = 0.25;
+    static final double DEFAULT_TMIN   = 0.15;
+    static final double DEFAULT_TMAX   = 1.00;
+    static final double DEFAULT_P_CHANGE = 0.00;
+    static final double DEFAULT_I_CHANGE = 0.02;
+    static final double DEFAULT_D_CHANGE = 0.00;
+    static final double DEFAULT_TARGET   = 0.75;
+    double ticks = 0, ticks2 = 0;
+    double time = 0, time2 = 0;
 
-    protected double maxPower;
-    protected double distance;
 
-    protected Ramper ramper;
+    public PID pid;
 
-    public PIDShootStep(double distance, double maxPower)
+    public PIDShootStep()
     {
-        this.distance = distance;
-        this.maxPower = maxPower;
+        //this. _.______ = _.______
     }
 
     @Override
@@ -28,21 +31,15 @@ public class PIDShootStep extends Step {
         super.start(r);
         robot.resetDriveMotors();
         robot.resetDriveEncoders();
-        ramper = new Ramper(DEFAULT_TUP, DEFAULT_TDOWN, distance, DEFAULT_MIN, maxPower);
+        pid = new PID(DEFAULT_TMAX, DEFAULT_TMIN, DEFAULT_P_CHANGE, DEFAULT_I_CHANGE, DEFAULT_D_CHANGE);
     }
 
     @Override
     public void run() {
         super.run();
-        double driveEncoderValue = robot.getDriveEncoderValue();
-        if (driveEncoderValue >= distance) {
-            stop();
-        }
-        else {
-            double power = ramper.getRampValue(driveEncoderValue);
-            robot.driveStraight(power);
-            tell("Encoder=%.2f, Power=%.2f", driveEncoderValue, power);
-        }
+        ticks = robot.getDriveEncoderValue();
+        time = System.currentTimeMillis();
+        pid.getNewSpeed((int)ticks, (int)ticks2, time, time2, DEFAULT_TARGET);
     }
     @Override
     public void stop(){

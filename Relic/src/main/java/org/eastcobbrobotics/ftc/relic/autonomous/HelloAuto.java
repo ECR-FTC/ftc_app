e06a9fd6a1849db07496f87efe87e52ec78c9565
ECR FTC Programming Team <ecrftc@gmail.com>
2017-10-15 14:43:04 -0400

    Added a basic StepRunner Routine - VM

--- Relic/src/main/java/org/eastcobbrobotics/ftc/relic/autonomous/HelloAuto.java
+++ Relic/src/main/java/org/eastcobbrobotics/ftc/relic/autonomous/HelloAuto.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.eastcobbrobotics.ftc.ecrlib.steprunner.DriveStep;
 import org.eastcobbrobotics.ftc.ecrlib.steprunner.Step;
 import org.eastcobbrobotics.ftc.ecrlib.steprunner.UntilAllDoneStep;
+import org.eastcobbrobotics.ftc.ecrlib.steprunner.UntilOneDoneStep;
 import org.eastcobbrobotics.ftc.relic.RelicBot;
 
 /**
@@ -15,19 +16,21 @@ import org.eastcobbrobotics.ftc.relic.RelicBot;
  */
 
 @Autonomous(name = "HelloAuto", group = "Testing")
+//@Disabled
+
 public class HelloAuto extends StepAutoCore {
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         RelicBot robot = new RelicBot();
-
-        Step mainStep = new UntilAllDoneStep(
+        Step mainStep = new UntilOneDoneStep(
                 waitFor(2000),
                 new DriveStep(0.8)
         );
 
         runStepAutonomous("HelloAuto", robot, mainStep);
+
     }
 
 }

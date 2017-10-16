29785f7251f1666c8765d1d371a1d3804ba8c858
ECR FTC Programming Team <ecrftc@gmail.com>
2017-10-16 18:33:17 -0400

    Various updated sensor codes.

---
+++ Relic/src/main/java/org/eastcobbrobotics/ftc/relic/autonomous/Steps/ReadColorSensorStep.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.autonomous.steps;
+
+import org.eastcobbrobotics.ftc.ecrlib.steprunner.Step;
+import org.firstinspires.ftc.teamcode.MorganaBot;
+
+import static org.eastcobbrobotics.ftc.ecrlib.steprunner.StepRobot.NONE_SEEN;
+
+/**
+ * Created by ECR_FTC on 3/10/17.
+ *
+ * Control step to find a red or blue light and set the flag "colorFound" to 1 (red) or 2 (blue).
+ *
+ * TODO: specify name of flag to set
+ */
+
+public class FindRedBlueStep extends Step {
+
+    @Override
+    public void run() {
+        super.run();
+        int color = ((MorganaBot) robot).getColorSeen();
+        if (color != NONE_SEEN) {
+            tell("color detected: %d", color);
+            setFlag("colorFound", color);
+            stop();
+        }
+    }
+}

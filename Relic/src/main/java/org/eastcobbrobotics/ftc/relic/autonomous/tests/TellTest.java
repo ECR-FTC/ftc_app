d3953a2caf93e711e9eade35ca9525db58a71ae1
Jeff Garbers <jeff.garbers@eastcobbrobotics.org>
2017-11-29 19:21:58 -0500

    Remove unneeded imports

--- Relic/src/main/java/org/eastcobbrobotics/ftc/relic/autonomous/tests/TellTest.java
+++ Relic/src/main/java/org/eastcobbrobotics/ftc/relic/autonomous/tests/TellTest.java
@@ -1,17 +1,13 @@
 package org.eastcobbrobotics.ftc.relic.autonomous.tests;
 
-import android.util.Log;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-import org.eastcobbrobotics.ftc.ecrlib.steprunner.RamperDriveStep;
 import org.eastcobbrobotics.ftc.ecrlib.steprunner.SayStep;
-import org.eastcobbrobotics.ftc.ecrlib.steprunner.SequenceStep;
 import org.eastcobbrobotics.ftc.ecrlib.steprunner.Step;
 import org.eastcobbrobotics.ftc.relic.RelicBot;
 import org.eastcobbrobotics.ftc.relic.autonomous.StepAutoCore;
-//import org.eastcobbrobotics.ftc.relic.autonomous.Steps.TurnRightStep;
 
 /**
  * ECR FTC 11096 - 2017 - 2018 Relic Recovery
